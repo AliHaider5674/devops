d7f78c4117139cedc4db131a8149662ee9d23295
AliHaider5674 <106062419+AliHaider5674@users.noreply.github.com>
2022-06-08 20:12:20 +0500

    Update Jenkinsfile.groovy

--- vars/Jenkinsfile.groovy
+++ vars/Jenkinsfile.groovy
@@ -1,11 +1,17 @@
 /*@Library('my-shared-library2')_*/
-
-pipeline {
+def call (body){
+    def config =[:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    
+    pipeline {
     agent any
     stages {
         stage('Build') {
             steps {
                 echo"{config.message}"
+                }
             }
         }
     }
